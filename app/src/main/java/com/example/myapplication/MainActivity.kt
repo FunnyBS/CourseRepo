c36cdda7262d4c62d58ae63a96804a4f1e2ca75b
Pavlo Deineha <funnybs@pavlos-air.intern>
2024-02-12 22:33:08 +0100

    continue to learn

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,26 +7,78 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import com.example.myapplication.data.RetrofitInstance
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
-class MainActivity : ComponentActivity() {
+const val TAG = "Coursera"
 
-    val TAG = "Coursera"
+@Composable
+fun DishItem(text: String) {
+    Text(text = text, modifier = Modifier.fillMaxWidth())
+}
 
-    @SuppressLint("CoroutineCreationDuringComposition")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-//            MyNav()
-            GlobalScope.launch {
-                delay(4000L)
-                Log.d(TAG, "Coroutine says hello from thread ${Thread.currentThread().name}")
+@SuppressLint("CoroutineCreationDuringComposition")
+@Composable
+fun TodoList() {
+    val todosState = remember { mutableStateOf("") }
+
+    LaunchedEffect(LocalLifecycleOwner.current.lifecycleScope) {
+        val response = try {
+            RetrofitInstance.api.getDishes()
+        } catch (e: IOException) {
+            Log.e(TAG, "IOException, you might not have internet connection")
+            return@LaunchedEffect
+        } catch (e: HttpException) {
+            Log.e(TAG, "HttpException, unexpected response")
+            return@LaunchedEffect
+        }
+
+        if (response.isSuccessful && response.body() != null) {
+            todosState.value = response.body().toString()!!
+            Log.d(TAG, "Successful")
+        } else {
+            Log.e(TAG, "Response not successful")
+        }
+    }
+
+    LazyColumn(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        userScrollEnabled = true
+    ) {
+        items(1) {
+            todosState.value.forEach { todo ->
+
+                DishItem(text = todo.toString())
             }
         }
-        Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
     }
 }
 
+@Composable
+fun MyApp() {
+    TodoList()
+}
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/data/dishItem.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.data
+
+data class dishItem(
+    val Appetizers: Appetizers,
+    val Dessert: Dessert,
+    val Drinks: Drinks,
+    val Salads: Salads
+)
